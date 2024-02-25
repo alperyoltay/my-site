11275fd235aabc4f2458c3a9104e640908e51510
Alper <118008590+alperyoltay@users.noreply.github.com>
2024-02-25 23:41:23 +0300

    Update username_generator.py

--- username_generator.py
+++ username_generator.py
@@ -1,8 +1,8 @@
 import random, string
 
 output_file = "usernames.txt"
-amount = int(input("\n kaç tane oluscak: "))
-character_amount = int(input("kaç karakter: "))
+amount = int(input("\n How many: "))
+character_amount = int(input("How many characters: "))
 
 for i in range(amount):
     generated = ("").join(random.choices(string.ascii_letters + string.digits, k = character_amount))
